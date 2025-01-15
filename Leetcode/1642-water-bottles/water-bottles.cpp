9b1e183277037d915649c4b3c770cbce79757d33
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2025-01-15 16:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (65.56%) - LeetSync

---
+++ Leetcode/1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+         int ans = numBottles;
+         // loop begin
+
+        while(numBottles>=numExchange){
+            int changed = numBottles/numExchange;
+            int rem = numBottles%numExchange;
+            ans += changed ;
+            numBottles = changed + rem;
+        }
+        return ans ;
+        //ends
+    }
+};
