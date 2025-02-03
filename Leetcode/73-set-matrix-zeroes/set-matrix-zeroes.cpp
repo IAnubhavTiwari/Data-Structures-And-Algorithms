03235edef7f2d64e6d3f9a60d407618c016a5fae
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2025-02-03 21:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (79.18%) - LeetSync

---
+++ Leetcode/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+         int col=matrix[0].size();
+        int row=matrix.size();
+        // std:: cout<<row<<"X"<<col;
+        // int arr[row][col];
+        for(int i=0; i<row; i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    // int k=i;
+                    for(int h=0; h<col; h++){
+                        if(matrix[i][h]!=0){
+                        matrix[i][h]=-999;
+                        }
+                    }
+                    for(int h=0; h<row; h++){
+                        if(matrix[h][j]!=0){
+                        matrix[h][j]=-999;
+                        }
+                    }
+
+                }
+            }
+        }
+        
+        for(int i=0; i<row;i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j]==-999){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+    }
+};
