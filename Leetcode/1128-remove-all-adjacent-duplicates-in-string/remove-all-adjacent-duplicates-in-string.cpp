797eb48172c6b65458cf5f96c57b5c0e8bf5f743
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2025-02-28 22:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (73.41%) - LeetSync

--- Leetcode/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ Leetcode/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string removeDuplicates(string s) {
         string ans="";
         int i=0;
-
+//
         while(i< s.length()){
 
             if(ans.length()>0 && ans[ans.length()-1] == s[i]){
