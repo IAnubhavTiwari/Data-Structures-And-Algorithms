0fd4c31ae53e3460e17a16a85a966ad0aa09276e
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2025-03-11 21:01:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input/taking-input.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void inputData() {
+        
+        int a;
+    string b;
+
+    // Read inputs
+    cin >> a;
+    cin.ignore(); // To ignore the newline character after integer input.
+    getline(cin, b);
+
+    // Print the outputs
+    cout << a << " " << b << endl;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        Solution obj;
+
+        obj.inputData();
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
